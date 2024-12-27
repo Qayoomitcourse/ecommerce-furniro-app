ff5b93ae528c2546ee7f02a2f40a98b981fe057c
qayoompk <qayoompk@gmail.com>
2024-12-27 18:19:55 +0500

    added cart page

---
+++ src/app/cartpage/page.tsx
@@ -0,0 +1,20 @@
+import Cart from '@/Components/cart'
+import React from 'react'
+
+const CartPage = () => {
+  return (
+    <div>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+This is Cart Page
+
+<Cart/>
+    </div>
+  )
+}
+
+export default CartPage
