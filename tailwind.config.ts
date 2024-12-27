20bf8f59c223ada5ad6cd57c63fec70e253adf3a
qayoompk <qayoompk@gmail.com>
2024-12-27 23:11:37 +0500

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
-import type { Config } from "tailwindcss";
-
-export default {
+module.exports = {
+  mode: 'jit',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -15,4 +14,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+}
